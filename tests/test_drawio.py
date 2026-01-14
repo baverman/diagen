21df38e64b9ad6bf921f8ac149a83baf409fbff9
Anton Bobrov <baverman@gmail.com>
2026-01-14 05:48:59 +0000

    implicit render in tests

--- tests/test_drawio.py
+++ tests/test_drawio.py
@@ -1,25 +1,35 @@
-from typing import Callable
+from typing import Iterator
 
 import pytest
 
 from diagen import drawio, stack, vstack
-from diagen.nodes import Node
+from diagen.nodes import Node, _children_stack
 from diagen.shapes import c4
-
-RenderFn = Callable[[str, Node], None]
 
 
 @pytest.fixture
-def render(tmp_path_factory: pytest.TempPathFactory) -> RenderFn:
-    def inner(fname: str, s: Node) -> None:
-        with open(tmp_path_factory.getbasetemp() / fname, 'w') as f:
-            f.write(drawio.render(s, compress=False))
+def render(
+    request: pytest.FixtureRequest, tmp_path_factory: pytest.TempPathFactory
+) -> Iterator[None]:
+    marker = request.node.get_closest_marker('fname')
+    nodes: list[Node]
+    _children_stack[:] = [nodes := []]
 
-    return inner
+    yield
+
+    if marker:
+        fname = marker.args[0]
+    else:
+        fname = request.node.name.removeprefix('test_') + '.drawio'
+
+    with open(tmp_path_factory.getbasetemp() / fname, 'w') as f:
+        f.write(drawio.render(nodes[0], compress=False))
+
+    _children_stack.clear()
 
 
-def test_full(render: RenderFn) -> None:
-    with stack['p-12 gap-24'] as s:
+def test_showcase(render: None) -> None:
+    with stack['p-12 gap-24']:
         with vstack['gap-40']:
             ext_user = c4.ExtPerson('Customer')
             ext_host = c4.ExtSystem("Customer's\nserver")
@@ -40,11 +50,9 @@ def test_full(render: RenderFn) -> None:
     )
     c4.edge['label-66'](packages.t[0.2], portal.b[0.2], 'Pushes usage data', 'Postgres')
 
-    render('showcase.drawio', s)
 
-
-def test_c4_shapes(render: RenderFn) -> None:
-    with c4.Boundary['dv gap-8 grid-3 items-align-start items-valign-end']('Boundary') as s:
+def test_c4_shapes(render: None) -> None:
+    with c4.Boundary['dv gap-8 grid-3 items-align-start items-valign-end']('Boundary'):
         with stack['col-1: gap-8']:
             c4.Person('Person')
             c4.ExtPerson('Ext person')
@@ -58,5 +66,3 @@ def test_c4_shapes(render: RenderFn) -> None:
         c4.Bus['valign-start']('Bus')
 
         c4.Component['col-1: align-end']('Component')
-
-    render('c4-shapes.drawio', s)
